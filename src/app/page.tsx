6b1b34f6bbd5932d93705460b13daed8ab86d892
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-08-09 23:45:28 +0300

    chore: create template main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main className={styles.main}>
-            <Link href={'/'}>start</Link>
+            <Link href={'/pages/first'}>start</Link>
         </main>
     );
 }
