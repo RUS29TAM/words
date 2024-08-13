a174e37d616eef7b95a5a7db83e713b925ce9edd
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-08-13 23:41:24 +0300

    chore: add routes

---
+++ form-backend/routes/formRoutes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import FormData from '../models/FormData';
+
+const router = express.Router();
+
+router.post('/submit', async (req, res) => {
+    try {
+        const { author, word, description } = req.body;
+        const newFormData = new FormData({ author, word, description });
+        await newFormData.save();
+        res.status(200).send('Данные успешно сохранены!');
+    } catch (error) {
+        res.status(500).send('Ошибка при сохранении данных.');
+    }
+});
+
+export default router;
